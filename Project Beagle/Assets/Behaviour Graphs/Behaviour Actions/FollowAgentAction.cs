a66c6467a5379a1f659699c5e967b1ac46f81df3
t0astie <jakob.w.lucas@gmail.com>
2025-02-28 01:14:16 +1100

    Add point between line function

--- Project Beagle/Assets/Behaviour Graphs/Behaviour Actions/FollowAgentAction.cs
+++ Project Beagle/Assets/Behaviour Graphs/Behaviour Actions/FollowAgentAction.cs
@@ -8,6 +8,28 @@ using UnityUtils;
 using System.Linq;
 using Unity.VisualScripting;
 
+public static class PathMath
+{
+    const float Epsilon = 0.01f;
+    
+    public static bool IsPointBetweenPoints(Vector2 a, Vector2 b, Vector2 point)
+    {
+        if (Mathf.Approximately(a.x, b.x))
+            return WithinBounds(a, b, point) && 
+                Mathf.Abs(point.x - a.x) < Epsilon;
+            
+        float slope = (b.y - a.y) / (b.x - a.x);
+        float intercept = a.y - slope * a.x;
+        float calculatedY = slope * point.x + intercept;
+
+        return WithinBounds(a, b, point) &&
+            Mathf.Abs(point.y - calculatedY) < Epsilon;
+    }
+
+    public static bool WithinBounds(Vector2 a, Vector2 b, Vector2 point) => point.x > Mathf.Min(a.x, b.x) && point.x < Mathf.Max(a.x, b.x) &&
+        point.y > Mathf.Min(a.y, b.y) && point.y < Mathf.Max(a.y, b.y);
+}
+
 public class Point
 {
     public Vertex Vertex;
@@ -34,93 +56,24 @@ public partial class FollowAgentAction : Action
     Vertex COrigin;
     public Queue<Point> Stack = new Queue<Point>();
 
-    Vertex[] Ends = new Vertex[2];
-
     protected override Status OnStart()
     {
         Target = Agent.Value.Follow.Target;
-
-        if (!Target.Heading || Target.Origin.Room == Target.Heading.Room)
-        {
-            Ends = new Vertex[2] { Target.Room.Vertices.First(), Target.Room.Vertices.Last() };
-        }
-        else
-        {
-            Ends = new Vertex[2] { Target.Origin, Target.Heading };
-        }
-
-        Stack.Enqueue(new Point(Ends[0]));
-        //Stack.Enqueue(Ends[1]);
-
-        COrigin = Target.Origin;
+        Stack.Enqueue(new Point(Target.Origin));
 
         return Status.Running;
     }
-
     protected override Status OnUpdate()
     {
         Agent.Value.GetNextHeading();
-
         MoveAlongStack(Agent.Value.Follow.HardDistance);
 
-        if (Target.Origin == COrigin) return Status.Running;
+        if (Target.Origin == COrigin) 
+            return Status.Running;
 
-        if (Target.Origin.IsPointer)
-        {
-            if (Stack.Count > 0 && Stack.Last().IsPointer)
-            {
-                if (Vector2.Distance(Target.Origin.Position, Stack.ElementAt(1).Position) > 
-                Vector2.Distance(Stack.Last().Position, Stack.ElementAt(1).Position))
-                {
-                    Stack.Dequeue();
-                    Stack.Enqueue(new Point(Target.Origin));
-                }
-            }
-            else
-            {
-                Stack.Enqueue(new Point(Target.Origin));
-            }
-        }
-        else if (Stack.Count > 0 && Stack.Last().IsPointer)
-        {
-            Stack.Dequeue();
-        }
-
-        if (Target.Origin.IsStation)
-        {
-            if (Stack.Count > 0 && Stack.Last().IsStation)
-            {
-                if (Vector2.Distance(Target.Origin.Position, Stack.ElementAt(1).Position) > 
-                Vector2.Distance(Stack.Last().Position, Stack.ElementAt(1).Position))
-                {
-                    Stack.Dequeue();
-                    Stack.Enqueue(new Point(Target.Origin));
-                }
-            }
-            else
-            {
-                Stack.Enqueue(new Point(Target.Origin));
-            }
-        }
-        else if (Stack.Count > 0 && Stack.Last().IsStation)
-        {
-            Stack.Dequeue();
-        }
-
-        if (Stack.Count > 10)
-        {
-            Vertex vertex = Stack.Dequeue().Vertex;
-            vertex.GetComponent<SpriteRenderer>().color = Color.gray;
-        }
-        
-        Stack.Enqueue(new Point(Target.Origin));
-        
-        foreach (Point p in Stack)
-        {
-            p.Vertex.GetComponent<SpriteRenderer>().color = Color.red;
-        }
-
-        COrigin = Target.Origin;
+        UpdatePathStack();
+        VisualizePath();
+        MaintainStackSize();
 
         return Status.Running;
     }
@@ -133,7 +86,7 @@ public partial class FollowAgentAction : Action
         float segmentDistance = Vector2.Distance(Target.transform.position, origin.Position);
 
         if (Vector2.Distance(Target.transform.position, Agent.Value.Pointer.Position) < remainingDistance && 
-                POLine(Target.transform.position, origin.Position, Agent.Value.Pointer.Position)) return;
+                PathMath.IsPointBetweenPoints(Target.transform.position, origin.Position, Agent.Value.Pointer.Position)) return;
 
         if (remainingDistance - segmentDistance <= 0)
         {
@@ -153,9 +106,9 @@ public partial class FollowAgentAction : Action
             segmentDistance = Vector2.Distance(current.Position, next.Position);
 
             if (Vector2.Distance(current.Position, Agent.Value.Pointer.Position) < remainingDistance && 
-                POLine(current.Position, next.Position, Agent.Value.Pointer.Position)) return;
+                PathMath.IsPointBetweenPoints(current.Position, next.Position, Agent.Value.Pointer.Position)) return;
             
-            if (remainingDistance - segmentDistance <= 0)
+            if (remainingDistance <= segmentDistance)
             {
                 Vector2 direction = (next.Position - current.Position).normalized;
                 Vector2 newPosition = current.Position + direction * remainingDistance;
@@ -166,36 +119,49 @@ public partial class FollowAgentAction : Action
             remainingDistance -= segmentDistance;
         }
 
-        if (remainingDistance > 0)
+        Agent.Value.SetPointer(Target.Room, Stack.First().Position);
+    }
+
+    void UpdatePathStack()
+    {
+        Point newPoint = new Point(Target.Origin);
+        
+        if (Stack.Count > 0)
         {
-            Agent.Value.SetPointer(Target.Room, Stack.ElementAt(0).Position);
+            Point last = Stack.Last();
+            bool shouldReplace = (Target.Origin.IsPointer || Target.Origin.IsStation) && 
+                                Vector2.Distance(newPoint.Position, Stack.ElementAt(1).Position) > 
+                                Vector2.Distance(last.Position, Stack.ElementAt(1).Position);
+
+            if (shouldReplace)
+            {
+                Stack.Dequeue();
+                Stack.Enqueue(newPoint);
+                return;
+            }
+        }
+
+        Stack.Enqueue(newPoint);
+    }
+
+    void VisualizePath()
+    {
+        foreach (Point p in Stack)
+        {
+            p.Vertex.GetComponent<SpriteRenderer>().color = 
+                (p.IsPointer || p.IsStation) ? Color.blue : Color.red;
         }
     }
 
-    bool POLine(Vector2 pos1, Vector2 pos2, Vector2 point)
+    void MaintainStackSize()
     {
-        float m = (pos2.y - pos1.y) / (pos2.x - pos1.x);
-        float c = pos1.y - m * pos1.x;
-
-        return point.y == m * point.x + c;
-    }
-
-    void SetVertexColor()
-    {
-        if (Ends[0]) Ends[0].GetComponent<SpriteRenderer>().color = Color.gray;
-        if (Ends[1]) Ends[1].GetComponent<SpriteRenderer>().color = Color.gray;
-
-        if (!Target.Heading || Target.Origin.Room == Target.Heading.Room)
+        while (Stack.Count > 10)
         {
-            Ends = new Vertex[2] { Target.Room.Vertices.First(), Target.Room.Vertices.Last() };
+            Point removed = Stack.Dequeue();
+            removed.Vertex.GetComponent<SpriteRenderer>().color = Color.gray;
         }
-        else
-        {
-            Ends = new Vertex[2] { Target.Origin, Target.Heading };
-        }
-
-        Ends[0].GetComponent<SpriteRenderer>().color = Color.blue;
-        Ends[1].GetComponent<SpriteRenderer>().color = Color.red;
+        
+        COrigin = Target.Origin;
     }
 }
 
