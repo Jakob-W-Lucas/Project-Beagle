b9d3d6f8eb3a9d88f07b127a936f555d40aca8e0
t0astie <jakob.w.lucas@gmail.com>
2025-03-06 17:57:47 +1100

    Add path extension class

---
+++ Project Beagle/Assets/Scripts/Extension Methods/PathExtensions.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+[System.Serializable]
+public class Point
+{
+    public Vertex Vertex;
+    public Vector2 Position;
+    public bool IsPointer;
+    public bool IsStation;
+
+    public Point(Agent a)
+    {
+        Vertex = a.Origin;
+        Position = Vertex.Position;
+        IsPointer = Vertex.IsPointer;
+        IsStation = Vertex.IsStation;
+    }
+}
+
+namespace UnityUtils {
+    public static class PathExtensions {
+        
+        public static Route CompareRoutes(Route route, Route other) => route.CompareTo(other) == 1 ? route : other;
+
+        const float Epsilon = 0.01f;
+            
+        public static bool IsPointBetweenPoints(Vector2 a, Vector2 b, Vector2 point)
+        {
+            if (Mathf.Approximately(a.x, b.x))
+                return WithinBounds(a, b, point) && 
+                    Mathf.Abs(point.x - a.x) < Epsilon;
+                
+            float slope = (b.y - a.y) / (b.x - a.x);
+            float intercept = a.y - slope * a.x;
+            float calculatedY = slope * point.x + intercept;
+
+            return WithinBounds(a, b, point) &&
+                Mathf.Abs(point.y - calculatedY) < Epsilon;
+        }
+
+        public static bool WithinBounds(Vector2 a, Vector2 b, Vector2 point) => point.x > Mathf.Min(a.x, b.x) && point.x < Mathf.Max(a.x, b.x) &&
+            point.y > Mathf.Min(a.y, b.y) && point.y < Mathf.Max(a.y, b.y);
+    }
+}
